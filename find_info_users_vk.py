aa7d5473623f905865ce184ec63b23fb902e1add
Eugeniy <zhenya.0295@mail.ru>
2023-09-13 22:39:55 +0300

    init telegram project

--- find_info_users_vk.py
+++ find_info_users_vk.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 
 def getData(vkId):
@@ -6,8 +7,7 @@ def getData(vkId):
     https1 = 'https://api.vk.com/method/users.get?user_ids='
     https2 = '&fields=bdate&access_token='
     https3 = '&v=5.131'
-    access_token = 'vk1.a.yKxxTovFfRF_K3RcLpZWBnO3N62ukrfVwgxAcebEakPJrUXReyvPDEX_z5lTNNwoUwKCPn98qWDGmvOawpialEJPDMQrIjQHE-2FDLqXBRZpRNzIrpCxP8fWpIV3zp26Uv5ncOPt4uwI_ANsItUcTGrnZ2ijCzbPDLL57LdzlNB_8mQ9nYR6dD1Dh90DQ0jZ'
-    response = requests.get(https1 + vkId + https2 + access_token + https3)
+    response = requests.get(https1 + vkId + https2 + f'{os.environ}' + https3)
     response = response.json()
     data = f"{vkId}: "
     for element in response['response']:
